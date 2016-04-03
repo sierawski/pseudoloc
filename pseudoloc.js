a195f271e76c9027440cf65f302a859efcd89138
sierawski <rafal.sierawski@gmail.com>
2016-04-03 21:08:59 +0200

    Improve delimiter regex
    Allow other characters than spaces and words when searching for
    delimiters.

--- pseudoloc.js
+++ pseudoloc.js
@@ -71,7 +71,7 @@ pseudoloc = function() {
     return pStr;
   };
   pseudoloc.str = function(str) {
-    var opts = pseudoloc.option, startdelim = opts.startDelimiter || opts.delimiter, enddelim = opts.endDelimiter || opts.delimiter, re = new RegExp(startdelim + "\\s*[\\w\\.\\s*]+\\s*" + enddelim, "g"), m, tokens = [], i = 0, tokenIdx = 0, result = "", c, pc;
+    var opts = pseudoloc.option, startdelim = opts.startDelimiter || opts.delimiter, enddelim = opts.endDelimiter || opts.delimiter, re = new RegExp(startdelim + ".+?" + enddelim, "g"), m, tokens = [], i = 0, tokenIdx = 0, result = "", c, pc;
     while (m = re.exec(str)) {
       tokens.push(m);
     }
